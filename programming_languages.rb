ab3e2090a55c322ed44201ecc0c28aa0230f1524
Woody Lucas <wlucas0311@gmail.com>
2018-12-03 05:25:10 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,32 +1,3 @@
 def reformat_languages(languages)
-  {
-  :ruby => {
-    :type => "interpreted",
-    :style => [:oo]
-  },
-  :javascript => {
-    :type => "interpreted",
-    :style => [:oo, :functional]
-  },
-  :python => {
-    :type => "interpreted",
-    :style => [:oo]
-  },
-  :java => {
-    :type => "compiled",
-    :style => [:oo]
-  },
-  :clojure => {
-    :type => "compiled",
-    :style => [:functional]
-  },
-  :erlang => {
-    :type => "compiled",
-    :style => [:functional]
-  },
-  :scala => {
-    :type => "compiled",
-    :style => [:functional]
-  }
-}
+  
 end
